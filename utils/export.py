2d77c95f403565a58394539d57a5d4954b9cbda9
KISHORE282005 <kishorekanna948@gmail.com>
2026-09-25 15:39:14 +0530

    last change

--- utils/export.py
+++ utils/export.py
@@ -596,8 +596,8 @@ def write_overall_analysis_sheet(
         ("VA Utilisation %", overall.get("value_added_percent", _percent(va_time, accounted))),
         ("NVA Utilisation %", overall.get("non_value_added_percent", _percent(nva_time, accounted))),
         ("Cycle Time (sec)", round(overall.get("cycle_time_seconds", total_time) or 0, 3)),
-        ("Per Day Output -  va_output),
-        ("Per Day Output -  overall_output)
+        ("Per Day Output - VA", va_output),
+        ("Per Day Output - Overall", overall_output)
     ]
 
     for label, value in metrics:
